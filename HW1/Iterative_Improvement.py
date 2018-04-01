70ef75fdcae658f83405e88a6b66316cbd5f5b95
Ron Chen <ucdj2003@gmail.com>
2018-04-02 02:22:43 +0800

    final version

--- HW1/Iterative_Improvement.py
+++ HW1/Iterative_Improvement.py
@@ -1,5 +1,6 @@
 import random
 import time
+import os
 
 # shuffling
 # def shuffling(arr, num_machine):
@@ -18,19 +19,47 @@ def swapping(arr, num_machine, num_jobs, swapjob, intervaljobs):
         arr[i][swapjob + intervaljobs], arr[i][swapjob] =  arr[i][swapjob], arr[i][swapjob + intervaljobs]
     return arr
 
-def ii():
-    optimal_ans = 10000000
+def ii(file_name):
+    # init var
+    num_machine = None
+    num_jobs = None
+    best_score = 9999
+    best_order = None
+    worst_score = 0
+    avg_score = 0
     swapjob = 0
     intervaljobs = 0
-    for n in range(0, 10000):
-        # Read tai.txt
-        with open('Homework/tai20_5_1.txt', 'rt', encoding='UTF-8') as fin:
+    iter_num = 10000
+
+    # read tai.txt
+    with open(file_name, 'rt', encoding='UTF-8') as fin:
+        str1 = fin.readline()
+        str1 = str1.split()
+        num_jobs = int(str1[0])
+        num_machine = int(str1[1])
+        arr = [[0 for j in range(num_jobs)] for i in range(num_machine)]   # arr[machines][jobs]
+        # read jobs
+        for i in range(num_machine):
+            str2 = fin.readline()
+            str2 = str2.split()
+            for j in range(num_jobs):
+                arr[i][j] = int(str2[j])
+
+    # print details
+    print("read: " + file_name)    
+    print("number of machines: " + str(num_machine))
+    print("number of jobs: " + str(num_jobs))
+    print("iterations: " + str(iter_num))
+
+    for iter_count in range(0, iter_num):
+        # read tai.txt
+        with open(file_name, 'rt', encoding='UTF-8') as fin:
             str1 = fin.readline()
             str1 = str1.split()
             num_jobs = int(str1[0])
             num_machine = int(str1[1])
             arr = [[0 for j in range(num_jobs)] for i in range(num_machine)]   # arr[machines][jobs]
-            # Read test number
+            # read jobs
             for i in range(num_machine):
                 str2 = fin.readline()
                 str2 = str2.split()
@@ -61,20 +90,30 @@ def ii():
             for j in range(1, num_jobs):
                 ans[i][j] = max(ans[i-1][j], ans[i][j-1]) + arr[i][j]
 
-        # local answer VS optimal answer        
-        local_ans = ans[num_machine-1][num_jobs-1]
-        print("The %d round answer = %d" % ((n+1), local_ans))
-        if local_ans < optimal_ans:
-            optimal_ans = local_ans
+        # Local Optima VS Best Optima        
+        score = ans[num_machine-1][num_jobs-1]
+        # print("The %d round answer = %d" % ((iter_count+1), score))
+        if score < best_score:
+            best_score = score
+            best_order = arr[num_machine-1]
+        if score > worst_score:
+            worst_score = score
+        avg_score += score
+    avg_score = avg_score / iter_num
 
-    print("Best Answer = %d" % (optimal_ans))
+    print("Best Score = %d" % (best_score))
+    print("Worst Score = %d" % (worst_score))
+    print("Avg Score = %d" % (avg_score))
+    print("best order is: ")
+    print(best_order)
 
 # main
 if __name__=="__main__":
-    start_time = time.time()
-    ii()
-    execute_time = time.time() - start_time
-    print("execute time: %.4fs\n" % (execute_time))
-
-# Best Answer = 1379
-# execute time: 10.8723s
+    # load all data
+    files = os.listdir("./HW1/data/")
+    # every file do ii
+    for i in files:
+        start_time = time.time()
+        ii("./HW1/data/" + i)
+        execute_time = time.time() - start_time
+        print("execute time: %.4fs\n" % (execute_time))
