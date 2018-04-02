7804e97509e79adf6087b24d63600a9ddadc2c5c
Ron Chen <ucdj2003@gmail.com>
2018-04-02 20:31:46 +0800

    Update Iterative_Improvement.py

--- HW1/Iterative_Improvement.py
+++ HW1/Iterative_Improvement.py
@@ -6,19 +6,37 @@ import os
 # def shuffling(arr, num_machine):
 #     for i in range(num_machine):
 #         random.choice(arr[i])
-#         print(arr[i])    
+#         print(arr[i])
 #     return arr
 
 # swap random two order
+
+temp0 = 0
+temp1 = 0
+
 def swap_order(arr, swap_num, num_machine, job_order):
-    job_order[swap_num[0]], job_order[swap_num[1]] = job_order[swap_num[1]], job_order[swap_num[0]]
+    job_order[swap_num[0]], job_order[swap_num[1]
+                                      ] = job_order[swap_num[1]], job_order[swap_num[0]]
+    temp0 = swap_num[0]
+    temp1 = swap_num[1]
     for i in range(num_machine):
-        arr[i][swap_num[0]], arr[i][swap_num[1]] = arr[i][swap_num[1]], arr[i][swap_num[0]]
+        arr[i][swap_num[0]], arr[i][swap_num[1]
+                                    ] = arr[i][swap_num[1]], arr[i][swap_num[0]]
     return arr, job_order
+
+def swap_original(arr, swap_num0, swap_num1, num_machine, job_order):
+    job_order[swap_num0], job_order[swap_num1
+                                      ] = job_order[swap_num1], job_order[swap_num0]
+    for i in range(num_machine):
+        arr[i][swap_num0], arr[i][swap_num1
+                                    ] = arr[i][swap_num1], arr[i][swap_num0]
+    return arr, job_order
+
 
 def make_new_order(arr, num_jobs, num_machine, job_order):
     random_number = random.sample(range(num_jobs), 2)
     return swap_order(arr, random_number, num_machine, job_order)
+
 
 def ii(file_name):
     # init var
@@ -28,6 +46,7 @@ def ii(file_name):
     best_order = None
     worst_score = 0
     avg_score = 0
+    local_score = 9999
     iter_num = 10000
 
     # read tai.txt
@@ -36,7 +55,8 @@ def ii(file_name):
         str1 = str1.split()
         num_jobs = int(str1[0])
         num_machine = int(str1[1])
-        arr = [[0 for j in range(num_jobs)] for i in range(num_machine)]   # arr[machines][jobs]
+        arr = [[0 for j in range(num_jobs)] for i in range(
+            num_machine)]   # arr[machines][jobs]
         # read jobs
         for i in range(num_machine):
             str2 = fin.readline()
@@ -44,44 +64,51 @@ def ii(file_name):
             for j in range(num_jobs):
                 arr[i][j] = int(str2[j])
 
-    temp_order = []
+    job_order = []
     for i in range(num_jobs):
-        temp_order.append(i)
+        job_order.append(i)
 
     # print details
-    print("read: " + file_name)    
+    print("read: " + file_name)
     print("number of machines: " + str(num_machine))
     print("number of jobs: " + str(num_jobs))
     print("iterations: " + str(iter_num))
 
-    for iter_count in range(0, iter_num):
-        job_order = []
-        # load previous order to job order
-        for i in temp_order:
-            job_order.append(i)
-
-        # Meta-heuristics Algorithm
+    for iter_count in range(0, 10):
         # DP(max span)
         ans = [[0 for j in range(num_jobs)] for i in range(num_machine)]
         ans[0][0] = arr[0][0]
         for i in range(1, num_machine):  # same jobs
-            ans[i][0] = ans[i-1][0] + arr[i][1] 
+            ans[i][0] = ans[i-1][0] + arr[i][1]
         for j in range(1, num_jobs):  # same machine
             ans[0][j] = ans[0][j-1] + arr[0][j]
         for i in range(1, num_machine):
             for j in range(1, num_jobs):
                 ans[i][j] = max(ans[i-1][j], ans[i][j-1]) + arr[i][j]
 
-        # Local Optima VS Best Optima        
+        # Local Optima VS Best Optima
         score = ans[num_machine-1][num_jobs-1]
+            
         # print("The %d round answer = %d" % ((iter_count+1), score))
         if score < best_score:
             best_score = score
             best_order = job_order
-            make_new_order(arr, num_jobs, num_machine, job_order)
         if score > worst_score:
             worst_score = score
         avg_score += score
+
+        # Meta-heuristics Algorithm
+        if score <= local_score:
+            make_new_order(arr, num_jobs, num_machine, job_order)
+            local_score = score
+        else:
+            swap_original(arr, temp0, temp1, num_machine, job_order)
+            make_new_order(arr, num_jobs, num_machine, job_order)
+        # print(arr[i])
+        # print(score)
+        # print(local_score)
+        
+
     avg_score = avg_score / iter_num
 
     print("Best Score = %d" % (best_score))
@@ -92,8 +119,9 @@ def ii(file_name):
     # print("Best arr is: ")
     # print(best_arr)
 
+
 # main
-if __name__=="__main__":
+if __name__ == "__main__":
     # load all data
     files = os.listdir("./HW1/data/")
     # every file do ii
